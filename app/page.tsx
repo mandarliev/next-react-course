2d0c57b86cdbe80b8cb46a7fd1455923715a2b02
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-11 16:03:43 +0200

    Async Components (Data Fetching) + PostgreSQL database set in railway.app with a couple of quiz entries

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,32 @@
-import Image from "next/image";
 import Link from "next/link";
+import postgres from "postgres";
+
+const sql = postgres(process.env.DATABASE_URL!);
+
+type Quiz = {
+  quiz_id: number;
+  quiz_title: string;
+};
+
+async function Quizzes() {
+  const quizzes: Quiz[] = await sql`SELECT * FROM quizzes`;
+
+  return (
+    <ul>
+      {quizzes.map((quiz) => (
+        <li key={quiz.quiz_id}>
+          <Link href={`/quiz/${quiz.quiz_id}`}>{quiz.quiz_title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function Home() {
   return (
     <section>
       <h1>All Quizzes</h1>
-      <ul>
-        <li>
-          <Link href="/quiz/1">Quiz 1</Link>
-        </li>
-        <li>
-          <Link href="/quiz/2">Quiz 2</Link>
-        </li>
-        <li>
-          <Link href="/quiz/3">Quiz 3</Link>
-        </li>
-      </ul>
+      <Quizzes />
     </section>
   );
 }
